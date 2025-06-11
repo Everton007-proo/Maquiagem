cf42ee6aded485027b973400698fbb758b25a90c
Everton007-proo <yagojefferson55@gmail.com>
2025-06-10 22:33:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// No futuro você pode adicionar interações aqui (ex: abrir um modal com mais informações)
+document.querySelectorAll('.more-info').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Mais informações dos produtos em breve!');
+  });
+});
